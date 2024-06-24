faae139f0065e402c64e250c59ee475f6d08d195
MrYadav2409 <akhileshyadav1821@gmail.com>
2024-06-24 18:55:03 +0530

    Add files via upload
    hibernate

--- Student.java
+++ Student.java
@@ -1,23 +1,35 @@
-package beans;
+package hibernatedemo1;
 
 public class Student {
-   private int rollNo;
-   private String sname;
-   
-   
-public Student(int rollNo, String sname) {
-	super();
-	this.rollNo = rollNo;
-	this.sname = sname;
-}
+	private int sid;
+	private String sname;
+	private float marks;
+	private String address;
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public float getMarks() {
+		return marks;
+	}
+	public void setMarks(float marks) {
+		this.marks = marks;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 
-public void show() 
-{
-  System.out.println("Roll number  :"  + rollNo);
-  System.out.println("Student name :"  + sname);
-}
-   
-   
-	
-	
 }
